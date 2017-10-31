a614224be7201893bedb8c7d0f9ea8426630788f
artyeug <artyeug@gmail.com>
2017-10-31 22:42:52 +0700

    Add an init version

---
+++ intermediatePrograms/LW06-T1-Main.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace LW06T1
+{
+    public class InvalidSexException : Exception
+    {
+        public InvalidSexException(string message) : base(message) {}
+    }
+
+    public class MainModule
+    {
+        /// <summary>
+        ///   The main entry point for the application
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            List<Pet> Pets = new List<Pet>();
+            try {
+                Pets.Add(new Dog("Polkan", 'm', 24.22, 48.1));
+                Pets.Add(new Cat("Dribble", 'm', 15.69, 30.12));
+                Pets.Add(new Parrot("Kesha", 'm', 2.34, 26));
+                Pets.Add(new Cat("Tabitha", 'f', 16.6, 29.45));
+                Pets.Add(new Dog("Boxer", 'm', 26.15, 49.01));
+            }
+            catch (InvalidSexException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            foreach (var pet in Pets) {
+                pet.Action();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ intermediatePrograms/LW06-T1-Pets.cs
@@ -0,0 +1,156 @@
+using System;
+
+namespace LW06T1
+{
+    public class Pet
+    {
+        protected static Random rand = new Random();
+
+        // Class members
+        //
+        private string name;
+        private char sex;
+        private double weight;
+        private double height;
+
+        // Getters/Setters
+        //
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public char Sex
+        {
+            get { return sex; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value > 0 ? value : 0; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value > 0 ? value : 0; }
+        }
+
+        // Constructor(s)
+        //
+        public Pet(string name, char sex, double weight, double height)
+        {
+            this.name = name;
+            if (sex != 'm' && sex != 'f') {
+                string errorInfo = "Additional info | Name: " + name + " | Tried sex declaration: " + sex;
+                throw new InvalidSexException(string.Format("An invalid sex definition has been declared. Only 'm' (male) or 'f' (female) are allowed to use. \n" + errorInfo));
+            }
+            else
+                this.sex = sex;
+            this.weight = weight;
+            this.height = height;
+        }
+
+        // Method(s)
+        //
+        public virtual void Action()
+        {
+            // smth here..
+        }
+    }
+
+    public class Cat : Pet
+    {
+        // Class member(s)
+        //
+        private const int lives = 9;
+
+        // Constructor
+        //
+        public Cat(string name, char sex, double weight, double height) : base(name, sex, weight, height) {}
+
+        // Class methods
+        //
+        public override void Action()
+        {
+            CatchMice();
+        }
+
+        public void CatchMice()
+        {
+            Console.WriteLine(Name + " is catching mice in the flat..");
+            string result = null, miceNumeral = "mice";
+            int miceCount = Pet.rand.Next(100);
+            if (miceCount == 0)
+                result = "There is nothing to say.";
+            else if (miceCount == 1)
+                miceNumeral = "mouse";
+            else if (miceCount < 20)
+                result = "Not bad, actually.";
+            else if (miceCount < 50)
+                result = "Pretty well, you know.";
+            else if (miceCount < 80)
+                result = "Hah, very good result!";
+            else
+                result = "Oh my god, this is incredible!";
+            Console.WriteLine("{0} {1} has catched {2} {3} in the flat.", result, Name, miceCount, miceNumeral);
+        }
+    }
+
+    public class Dog : Pet
+    {
+        // Constructor
+        //
+        public Dog(string name, char sex, double weight, double height) : base(name, sex, weight, height) {}
+
+        // Class methods
+        //
+        public override void Action()
+        {
+            Guard();
+        }
+
+        public void Guard()
+        {
+            Console.WriteLine(Name + " is guarding the perimeter..");
+            string[] famousGuests = { "Steve Jobs", "Mark Zuckerberg", "Elon Musk", "Bill Gates", "Ray Kurzweil", "John Lasseter", "Jony Ive" };
+
+            string guest = famousGuests[rand.Next(famousGuests.Length)];
+            if (guest == "Bill Gates")
+                FrightenTheGuest();
+            else
+                Console.WriteLine(Name + " has met no threat. The house was visited by {0}.", guest);
+        }
+
+        private void FrightenTheGuest()
+        {
+            Console.WriteLine("**Growl sounds** - looks like Bill Gates was trying to visit our house.");
+        }
+    }
+
+    public class Parrot : Pet
+    {
+        // Constructor
+        //
+        public Parrot(string name, char sex, double weight, double height) : base(name, sex, weight, height) {}
+
+        // Class methods
+        //
+        public override void Action()
+        {
+            Chatter();
+        }
+
+        public void Chatter()
+        {
+            string sex = null;
+            switch (Sex) {
+                case 'm':
+                    sex = "male"; break;
+                case 'f':
+                    sex = "female"; break;
+            }
+
+            Console.WriteLine("Hello, Mr. or Mrs.! My name is {0}, I am a parrot! I am a {1}, also my weight is {2} kg and my height is {3} cm!", Name, sex, Weight, Height);
+        }
+    }
+}
