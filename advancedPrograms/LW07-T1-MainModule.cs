fd03237d35117948bf9ddf586eba1b63a74a5fa1
artyeug <artyeug@gmail.com>
2017-11-09 23:58:41 +0700

    Add an empty body for QueueTesting method (that's funny)

--- advancedPrograms/LW07-T1-MainModule.cs
+++ advancedPrograms/LW07-T1-MainModule.cs
@@ -11,15 +11,13 @@ namespace LW07T1
 
         public static void ArrayTesting()
         {
-            var Afirst = new Array<int>();
-            Afirst.Add(1,2,3,4,5,6,7,8,9,10);
-            Afirst.Display();
+            var first = new Array<int>();
+            first.Add(1,2,3,4,5,6,7,8,9,10);
+            first.Display();
 
-            var Asecond = new Array<int>();
-            Asecond.Add(15,14,13,12,11,10,9,8,7,6,5,4,3,2,1);
-            Asecond.Display();
-            
-            // should test some implemented methods..
+            var second = new Array<int>();
+            second.Add(15,14,13,12,11,10,9,8,7,6,5,4,3,2,1);
+            second.Display();
         }
 
         public static void StackTesting()
@@ -27,6 +25,11 @@ namespace LW07T1
             var Sfirst = new Stack<int>();
             Sfirst.Push(17, 14, 32, 55, 1, 2, 3, 10, 16);
             Sfirst.Display();
+        }
+
+        public static void QueueTesting()
+        {
+            
         }
 
         public static void Main(string[] args)
