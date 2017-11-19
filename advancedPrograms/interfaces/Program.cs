9e8be8cc88dd1ac6a5a9ea3e9c4f5e4e7a09fdd4
artyeug <artyeug@gmail.com>
2017-11-19 22:53:29 +0700

    Add some classes testing

--- advancedPrograms/interfaces/Program.cs
+++ advancedPrograms/interfaces/Program.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 
 namespace interfaces
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine();
+            var students = new List<PartTimeStudent>
+            {
+                new PartTimeStudent("Tony Stark", 52, "MIT"),
+                new PartTimeStudent("Artem Piskarev", 19, "TUCSR", "KFC"),
+                new PartTimeStudent("Irina Iost", 18, "TPU", "Google")
+            };
+
+            foreach (var student in students)
+            {
+                student.DisplayInfo();
+                student.Study();
+                student.Work();
+            }
+            
+            
         }
     }
 }
