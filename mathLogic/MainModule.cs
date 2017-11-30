b9fa3a9b50ab483a6b03ff7a11603334afe3cc89
artyeug <artyeug@gmail.com>
2017-11-30 23:09:00 +0700

    Rename initial file

---
+++ mathLogic/MainModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace mathLogic
+{
+    public static class MainModule
+    {
+        public static void Main()
+        {
+            try
+            {
+                var collection = new Collection();
+                collection.Display();
+                
+                Console.WriteLine("Type number of program: ");
+                var chosenProgram = int.Parse(Console.ReadLine());
+                collection.Execute(chosenProgram);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
