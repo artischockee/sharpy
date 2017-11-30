b048f459355697d26f0095e06236274ff4d7f6c2
artyeug <artyeug@gmail.com>
2017-11-30 20:56:45 +0700

    Add new central file that executes all of the programs here

---
+++ mathLogic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mathLogic
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
