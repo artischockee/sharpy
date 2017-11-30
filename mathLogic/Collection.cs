723d3d99b1405a8eadde6e08c81b37c6ceeb0d84
artyeug <artyeug@gmail.com>
2017-11-30 23:12:05 +0700

    Add a separate file for 2 classes

---
+++ mathLogic/Collection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace mathLogic
+{
+    public abstract class Program
+    {
+        protected internal virtual void ShowName()
+        {
+            Console.WriteLine("An empty field (virtual method of ClassesContainer class)");
+        }
+
+        protected internal virtual void Execute()
+        {
+            Console.WriteLine("Warning: Nothing to execute from ClassesContainer class");
+        }
+    }
+    
+    public class Collection : Program, IEnumerable<Program>
+    {
+        private const int InitialIndexOfProgram = 1;
+        private readonly List<Program> _programs;
+
+        // Add new instances here
+        public Collection()
+        {
+            _programs = new List<Program>
+            {
+                new MainNormalForms(),
+                new MainTuringMachine()
+            };
+        }
+
+        public void Display()
+        {
+            var index = InitialIndexOfProgram;
+            foreach (var item in _programs)
+            {
+                Console.Write($"[{index++}] ");
+                item.ShowName();
+            }
+        }
+
+        public void Execute(int programIndex)
+        {
+            if (programIndex < InitialIndexOfProgram || programIndex > _programs.Count)
+                throw new ArgumentOutOfRangeException(nameof(programIndex));
+            
+            _programs[--programIndex].Execute();
+        }
+
+        IEnumerator<Program> IEnumerable<Program>.GetEnumerator()
+        {
+            return _programs.GetEnumerator();
+        }
+        
+        public IEnumerator GetEnumerator()
+        {
+            return _programs.GetEnumerator();
+        }
+    }
+}
