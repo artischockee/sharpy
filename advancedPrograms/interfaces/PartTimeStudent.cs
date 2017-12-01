7da5de48424b74e0398ce58c22f2b9be0ded3118
artyeug <artyeug@gmail.com>
2017-12-01 14:22:40 +0700

    Join 'name' and 'surname' field into one 'Name' field

--- advancedPrograms/interfaces/PartTimeStudent.cs
+++ advancedPrograms/interfaces/PartTimeStudent.cs
@@ -11,22 +11,16 @@ namespace interfaces
         private const int RequiredAge = 16;
         
         private readonly int _indexNumber;
-        private readonly string _name;
-        private readonly string _surname;
+        public override string Name { get; }
         
         private int _age;
         private string _placeOfWork;
-        private string _placeOfStudy;
-        
+        public string PlaceOfStudy { get; set; }
+
         // Properties:
         
         public static int AmountOfObjects => _amountOfObjects;
         public int IndexNumber => _indexNumber;
-        public string PlaceOfStudy
-        {
-            get { return _placeOfStudy; }
-            set { _placeOfStudy = value; }
-        }
 
         // Constructors:
 
@@ -35,18 +29,16 @@ namespace interfaces
             _amountOfObjects = 0;
         }
 
-        public PartTimeStudent(string fullName, int age, string placeOfStudy, string placeOfWork = null)
+        public PartTimeStudent(string name, int age, string placeOfStudy, string placeOfWork = null)
         {
-            if (fullName == null || placeOfStudy == null)
+            if (name == null || placeOfStudy == null)
                 throw new ArgumentNullException();
             if (age < RequiredAge)
                 throw new ArgumentOutOfRangeException($"'Age' must be greater than {RequiredAge}");
 
-            var name = fullName.Split();
-            _name = name[0];
-            _surname = name[1];
+            Name = name;
             _age = age;
-            _placeOfStudy = placeOfStudy;
+            PlaceOfStudy = placeOfStudy;
             _placeOfWork = placeOfWork ?? "none";
             
             _indexNumber = _amountOfObjects++;
@@ -57,7 +49,7 @@ namespace interfaces
         public override void Work()
         {
             if (_placeOfWork != "none")
-                Console.WriteLine($"Student {_name} {_surname} is doing some work at {_placeOfWork}");
+                Console.WriteLine($"Student {Name} is doing some work at {_placeOfWork}.");
         }
 
         public override string WriteReport()
@@ -65,20 +57,20 @@ namespace interfaces
             if (_placeOfWork == "none")
                 return null;
             
-            var workReport = $"A work report written by employee {_name} {_surname} (student)";
+            var workReport = $"A work report written by employee {Name} (student).";
             return workReport;
         }
 
         public void Study()
         {
-            Console.WriteLine($"Student {_name} {_surname} is studying.");
+            Console.WriteLine($"Student {Name} is studying.");
         }
 
         public void DisplayInfo()
         {
             Console.WriteLine("Info about part-time student:");
-            Console.WriteLine($"> Name: {_name} {_surname}, age: {_age};");
-            Console.WriteLine($"> Place of study: {_placeOfStudy}, job: {_placeOfWork}.");
+            Console.WriteLine($"> Name: {Name}, age: {_age};");
+            Console.WriteLine($"> Place of study: {PlaceOfStudy}, job: {_placeOfWork}.");
         }
     }
 }
