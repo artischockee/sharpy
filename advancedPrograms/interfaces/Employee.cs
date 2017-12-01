15b4686c8156ae1866bca1768e17ab61c05b1ebd
artyeug <artyeug@gmail.com>
2017-12-01 13:56:48 +0700

    Add a Sort delegate and appropriate method for it

--- advancedPrograms/interfaces/Employee.cs
+++ advancedPrograms/interfaces/Employee.cs
@@ -1,8 +1,26 @@
-﻿namespace interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.String;
+
+namespace interfaces
 {
+    public delegate void Sort(List<Employee> list);
+    
     public abstract class Employee
     {
+        public abstract string Name { get; }
         public abstract void Work();
         public abstract string WriteReport();
+
+        public static void SortTurnersByName(List<Employee> list)
+        {
+            var turners = list.Where(person => person is Turner).ToList();
+
+            turners.Sort((employee1, employee2) => CompareOrdinal(employee1.Name, employee2.Name));
+            
+            foreach (var person in turners)
+                Console.WriteLine(person.Name);
+        }
     }
 }
