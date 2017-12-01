14d0870dbaac92061512647d0b35d66e18e9075d
artyeug <artyeug@gmail.com>
2017-12-01 14:53:42 +0700

    Move 'Experiment' method to main class Program

--- advancedPrograms/interfaces/Program.cs
+++ advancedPrograms/interfaces/Program.cs
@@ -3,34 +3,37 @@ using System.Collections.Generic;
 
 namespace interfaces
 {
-    public static class Test
+    public static class Program
     {
-        public static void Experiment()
+        private static void Experiment()
         {
             var employees = new List<Employee>
             {
                 new Turner("John Travolta", 63, 6),
                 new Turner("Anthony Stark", 52, 3),
                 new PartTimeStudent("Jack Sparrow", 53, "Oxford University", "Hollywood"),
-                new PartTimeStudent("Keanu Reeves", 53, "КИПТСУ"),
-                new Turner("Bruce Willis", 62)
+                new PartTimeStudent("Keanu Reeves", 53, "KIPTSU", "KIPTSU"),
+                new Turner("Bruce Willis", 62),
+                new Turner("Christopher Nolan", 47),
+                new PartTimeStudent("Vladimir Putin", 65, "S-Petersburg State University", "Kremlin"),
+                new Turner("Arnold Schwarzenegger", 70, 6)
             };
 
             foreach (var employee in employees)
             {
+                employee.DisplayInfo();
                 employee.Work();
-                Console.WriteLine(employee.WriteReport());
             }
+
+            Sort sortTurners = Employee.SortTurnersByName;
+            sortTurners(employees);
         }
-    }
-    
-    public class Program
-    {
+        
         public static void Main(string[] args)
         {
             try
             {
-                Test.Experiment();
+                Experiment();
             }
             catch (Exception e)
             {
