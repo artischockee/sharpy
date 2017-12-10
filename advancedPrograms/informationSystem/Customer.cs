4ea400b11b8da02dc4e6a5bf35bb3eabd075d9e5
artyeug <artyeug@gmail.com>
2017-12-10 22:03:56 +0700

    Add correct version

--- advancedPrograms/informationSystem/Customer.cs
+++ advancedPrograms/informationSystem/Customer.cs
@@ -1,19 +1,52 @@
-﻿namespace informationSystem
+﻿using System;
+
+namespace informationSystem
 {
-    internal abstract class Customer
+    internal abstract class Customer : IDiscountAccessor
     {
-        // Abstract properties:
-
-        public abstract string Name { get; }
-        public abstract string Address { get; }
-        public abstract string Contacts { get; }
-        public abstract int YearsOfUsage { get; }
-        public abstract Tariff CurrentTariff { get; }
-        protected abstract double Discount { get; set; }
-        public abstract double MonthlyPayment { get; }
-
-        // Abstract methods:
+        // Fields (auto-properties):
         
-        public abstract void DisplayInfo();
+        public string Address { get; protected set; }
+        public int YearsOfUsage { get; protected set; }
+        public Tariff Tariff { get; protected set; }
+        public double Discount { get; protected set; }
+        public double MonthlyPayment { get; protected set; }
+
+        // Constructor:
+
+        protected Customer(
+            string address, int yearsOfUsage, Tariff tariff)
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new ArgumentNullException(nameof(address));
+            if (yearsOfUsage < 0)
+                throw new ArgumentOutOfRangeException(nameof(yearsOfUsage));
+
+            Address = address;
+            YearsOfUsage = yearsOfUsage;
+            Tariff = tariff;
+        }
+        
+        // Methods:
+        
+        public void SetDiscount(double discount)
+        {
+            if (discount < 0 || discount > 100)
+                throw new ArgumentOutOfRangeException();
+            
+            Discount = discount;
+            MonthlyPayment = Discount == 0
+                ? Tariff.PricePerMonth
+                : Tariff.PricePerMonth * ((100 - discount) / 100);
+        }
+        
+        // Virtual methods:
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Address: {Address}.");
+            Console.WriteLine($"Years of usage: {YearsOfUsage}; Tariff: {Tariff.TariffName}.");
+            Console.WriteLine($"Monthly payment: ${MonthlyPayment} ({Discount}% of discount).");
+        }
     }
 }
