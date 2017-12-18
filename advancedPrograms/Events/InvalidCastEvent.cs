8ebbcd32227b53f5d003df28596f4e0c07b4625b
artyeug <artyeug@gmail.com>
2017-12-18 12:05:38 +0700

    Add a new file

---
+++ advancedPrograms/Events/InvalidCastEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Events
+{
+    internal class InvalidCastEvent : InvalidCastException
+    {
+        public event InvalidCastEventHandler activate;
+
+        public InvalidCastEvent(string message = null) : base(message)
+        {
+            activate = Program.Handler;
+        }
+
+        public void Activate()
+        {
+            activate?.Invoke();
+        }
+    }
+}
