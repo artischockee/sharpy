2398f395c1d3e9099b332b091ea1df2390a3054b
artyeug <artyeug@gmail.com>
2017-12-18 12:05:49 +0700

    Complete the tasks

--- advancedPrograms/Events/Program.cs
+++ advancedPrograms/Events/Program.cs
@@ -3,25 +3,40 @@ using System.Collections.Generic;
 
 namespace Events
 {
+    internal delegate void InvalidCastEventHandler();
+    
     internal delegate void ProdDisplay();
     internal delegate void ProdMoveAction(float distance);
     internal delegate double ProdSelling();
     
     internal static class Program
     {
-        public static void Main(string[] args)
+        public static void Handler()
         {
-            var list = new List<Detail>
-            {
-                new Detail("Detail01", 14.00, 20.015),
-                new Mechanism("Mechanism01", 9.01, 0.0012, 3),
-                new Node("Node01", 10, 11.59, 5.5F),
-                new Product("Product01", "Toshiba", 29.22, 11.708)
-            };
+            Console.WriteLine("Seems like something went wrong.");
+        }
 
-            foreach (var detail in list)
-                detail.DisplayInfo();
+        private static void ThirdTask()
+        {
+            Console.WriteLine("Type amount of creatable details:");
             
+            try
+            {
+                int amount;
+                if (!int.TryParse(Console.ReadLine(), out amount))
+                    throw new InvalidCastEvent("This is a stupid example of the exception.");
+
+                var detailsList = new List<Detail>(amount);
+            }
+            catch (InvalidCastEvent e)
+            {
+                Console.WriteLine(e.Message);
+                e.Activate();
+            }
+        }
+        
+        private static void SecondTask()
+        {
             var spaceCannon = new Product("Space Cannon", "Truffade", 15.45, 30.00);
             const float distance = 15F; 
 
@@ -36,6 +51,28 @@ namespace Events
             movingAllAxes(distance);
             var receivedMoney = selling();
             Console.WriteLine($"You have received ${receivedMoney} for selling {spaceCannon.Name}.");
+            Console.ReadKey();
+        }
+        
+        private static void FirstTask()
+        {
+            var list = new List<Detail>
+            {
+                new Detail("Detail01", 14.00, 20.015),
+                new Mechanism("Mechanism01", 9.01, 0.0012, 3),
+                new Node("Node01", 10, 11.59, 5.5F),
+                new Product("Product01", "Toshiba", 29.22, 11.708)
+            };
+
+            foreach (var detail in list)
+                detail.DisplayInfo();
+        }
+        
+        public static void Main(string[] args)
+        {
+            FirstTask();
+            SecondTask();
+            ThirdTask();
         }
     }
 }
